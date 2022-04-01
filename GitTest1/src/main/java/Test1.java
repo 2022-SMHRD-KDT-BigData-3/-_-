378af570d5bb762bcb5f384ac2617b4273cced7e
smhrd <smhrd@DESKTOP-EGSRJ2C>
2022-04-01 12:31:33 +0900

    수정4

--- GitTest1/src/main/java/Test1.java
+++ GitTest1/src/main/java/Test1.java
@@ -3,10 +3,15 @@ public class Test1 {
 
 	public static void main(String[] args) {
 
+
 		System.out.println("첫번째 커밋");
 		System.out.println("두번째 커밋");
 		
-		System.out.println("서연");
+		System.out.println("모찌는 귀여워");
+		
+		int a = 18;
+		System.out.println(a);
+		
 	}
 
 }
